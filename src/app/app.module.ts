eaa4ca074c9dc1737796b12b60671cd17a69f442
Linda Rimoldi <linda.rimoldi@reti.it>
2020-02-03 10:21:16 +0100

    servizi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { MainPageComponent } from './mainpage/mainpage.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FooterComponent } from './footer/footer.component';
+import { ServiziComponent } from './servizi/servizi.component';
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     MainPageComponent,
     FooterComponent,
+    ServiziComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/servizi/servizi.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ServiziComponent } from './servizi.component';
+
+describe('ServiziComponent', () => {
+  let component: ServiziComponent;
+  let fixture: ComponentFixture<ServiziComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ServiziComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ServiziComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
