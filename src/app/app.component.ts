3e5e8329f70aabfbef0b1e8cb833491c1be4e632
Linda Rimoldi <linda.rimoldi@reti.it>
2020-05-11 12:43:07 +0200

    maps in dove siamo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,36 +6,11 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, AfterViewInit {
-  @ViewChild('mapContainer', { static: false }) gmap: ElementRef;
+export class AppComponent implements OnInit {
 
   title = 'Vertex';
-  map: google.maps.Map;
-  lat = 45.551798;
-  lng = 9.075016;
-
-  coordinates = new google.maps.LatLng(this.lat, this.lng);
-
-  mapOptions: google.maps.MapOptions = {
-    center: this.coordinates,
-    zoom: 19,
-  };
-  marker = new google.maps.Marker({
-    position: this.coordinates,
-    map: this.map,
-  });
 
   constructor() { }
   ngOnInit() {
-  }
-
-  ngAfterViewInit() {
-    this.mapInitializer();
-  }
-
-  mapInitializer() {
-    this.map = new google.maps.Map(this.gmap.nativeElement,
-      this.mapOptions);
-    this.marker.setMap(this.map);
   }
 }
