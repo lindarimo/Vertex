d40fa21f8e49ebb19f55a051d1585d5fc2396daa
Linda Rimoldi <linda.rimoldi@reti.it>
2020-06-03 12:18:37 +0200

    centro medico

--- src/app/components/servizi-detail/centromedico/centromedico.component.ts
+++ src/app/components/servizi-detail/centromedico/centromedico.component.ts
@@ -10,9 +10,44 @@ import { Specialista } from 'src/app/model';
 export class CentromedicoComponent implements OnInit {
 
   specialisti: Specialista[] = [];
+  public isCollapsed = false;
+  public detailToShow: string;
 
   constructor(private httpClient: HttpClient) { }
   ngOnInit() {
     window.scroll(0, 0);
+  }
+  onShowDetail(servizio: string) {
+    if (servizio !== this.detailToShow) {
+      switch (servizio) {
+        case 'fisioterapia': {
+          this.detailToShow = 'fisioterapia';
+          $('.background-imgholder').css('background-image', 'url("../../../../assets/images/fisioterapista.jpg")');
+          break;
+        }
+        case 'massoterapia': {
+          this.detailToShow = 'massoterapia';
+          $('.background-imgholder').css('background-image', 'url("../../../../assets/images/massoterapista.jpg")');
+          break;
+        }
+        case 'nutrizione': {
+          this.detailToShow = 'nutrizione';
+          $('.background-imgholder').css('background-image', 'url("../../../../assets/images/nutrizionista.jpg")');
+          break;
+        }
+        case 'ortopedia': {
+          this.detailToShow = 'ortopedia';
+          $('.background-imgholder').css('background-image', 'url("../../../../assets/images/ortopedico.jpg")');
+          break;
+        }
+        case 'osteopatia': {
+          this.detailToShow = 'osteopatia';
+          $('.background-imgholder').css('background-image', 'url("../../../../assets/images/osteopata.jpg")');
+          break;
+        }
+      }
+    } else {
+      this.detailToShow = undefined;
+    }
   }
 }
